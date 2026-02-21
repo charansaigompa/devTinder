583e762ab9027d5624cdad95e10ea8491fcd9473
charansaigompa <gompacharansai10@gmail.com>
2026-02-21 19:56:07 +0530

    worked with route params

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express=require("express")
 const app=express()
+ app.get("/user",(req,res)=>{
+    console.log(req.query)
+    res.send({firstname:"ramu",secondname:"krishna"})
+})
+ app.get("/user2/:id/:password",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"ramu",secondname:"krishna"})
+})
 
 
-app.use("/test",(req,res)=>{
-    res.send("i am testing now")
-})
-app.use("/hello",(req,res)=>{res.send("hello ")})
-app.use("/",(req,res)=>{
-    res.send("hello i am from dashboard")
-})
 app.listen(7777,()=>{
     console.log("server is running on port 7777")
 })
+  
