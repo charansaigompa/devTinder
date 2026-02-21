0d879189a303ff3794a94a6ea71fbd370227c3b1
charansaigompa <gompacharansai10@gmail.com>
2026-02-21 22:28:30 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
-app.use(
-  "/user",
-  (req, res,next) => {
-    next()
-    
-  },
-  (req, res,next) => {
-    next()
-   
-  },
-    (req, res) => {
-    res.send("3rd route handler");
-  },
- 
-);
+//Handling Auth middleware  for all GET POST,.. request
+const {adminAuth,userAuth}=require("./middleware/auth");
+app.use("/admin",adminAuth)
+app.get("/admin/getDetails",(req,res)=>{
+    res.send("All details are sent")
+})
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("User details are deleted")
+})
+app.post("/user/login",(req,res)=>{
+    res.send("you are logining in ")
+})
+app.get("/user/details",userAuth,(req,res)=>{
+    res.send("you are seeing user details")
+})
+
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
