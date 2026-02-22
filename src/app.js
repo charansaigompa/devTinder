9760d54ce9c70aa18d57e20000050999778ce318
charansaigompa <gompacharansai10@gmail.com>
2026-02-22 20:41:04 +0530

    dynamic data in singup api

--- src/app.js
+++ src/app.js
@@ -2,15 +2,9 @@ const express = require("express");
 const app = express();
 const connectDB=require("./config/database")
 const User=require("./models/user")
+app.use(express.json())
 app.post("/signup",async(req,res)=>{
-   const user=new User({
-    firstName:"Rama",
-    lastName:"Krishna",
-    emailId:"ramkrishna@email.com",
-    password:"ram123*",
-    age:21,
-    gender:"male",
-   })
+   const user=new User(req.body)
    try{
          await user.save()
    res.send("User Added Successfully")
