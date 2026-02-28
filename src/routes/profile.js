4f25896e4d17fce56e9af8c5d3ae17056d567c07
charansaigompa <gompacharansai10@gmail.com>
2026-02-28 22:22:23 +0530

    added edit profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,10 +2,11 @@ const express=require("express")
 const profileRouter=express.Router()
 const{userAuth}=require("../middleware/auth");
 const User = require("../models/user");
+const {validateEditProfileData}=require("../utils/validation")
 
 
 
-profileRouter.get("/profile",userAuth,async(req,res)=>{
+profileRouter.get("/profile/view",userAuth,async(req,res)=>{
   try{
      const user=req.user
       res.send(user)
@@ -13,6 +14,24 @@ profileRouter.get("/profile",userAuth,async(req,res)=>{
     res.send("ERROR "+err.message)
   }
   
+
+})
+profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
+  try{
+    if(!validateEditProfileData){
+    throw new Error("Invalid Edit")
+  }
+  const loggedInUser=req.user
+  Object.keys(req.body).forEach(key=>loggedInUser[key]=req.body[key])
+  await loggedInUser.save()
+  res.json({message:`${loggedInUser.firstName+" "+loggedInUser.lastName}, you profile is updated`,
+data:loggedInUser})
+
+
+  }
+  catch(err){
+    res.send("Error "+err.message)
+  }
 
 })
 
