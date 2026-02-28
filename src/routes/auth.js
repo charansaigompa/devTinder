553d6eb44a9b0d6a75b032dcccbee6027f59c89d
charansaigompa <gompacharansai10@gmail.com>
2026-02-28 22:22:01 +0530

    added logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -49,4 +49,10 @@ authRouter.post("/login",async(req,res)=>{
     res.send(err.message)
   }
 })
+
+authRouter.post("/logout",(req,res)=>{
+    res.cookie("token",null,{expires:new Date(Date.now())
+    })
+    res.send("logout successful!")
+})
 module.exports=authRouter;
