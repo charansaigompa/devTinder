800bdc9980403bd332516efc005ca5ee7222f16e
charansaigompa <gompacharansai10@gmail.com>
2026-02-28 22:22:41 +0530

    added validation to edit  profile api

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,18 +1,29 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+  const { emailId, password, firstName, lastName } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid ");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid ");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not valid");
+  }
+};
 
-const validator=require("validator")
-const validateSignUpData=(req)=>{
-const {emailId,password,firstName,lastName}=req.body;
-if(!firstName||!lastName){ 
-    throw new Error("Name is not valid ")
-}
-else if(!validator.isEmail(emailId)){
-    throw new Error("Email is not valid ")
-}
-else if(!validator.isStrongPassword(password)){
-    throw new Error("Password is not valid")
-}
-}
-
-module.exports={
-    validateSignUpData,
-}
+const validateEditProfileData = (req) => {
+  const allowedEditFeilds = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "about",
+    "skills",
+    "photoUrl",
+  ];
+  const isEditAllowed=Object.keys(req.body).every(key=>allowedEditFeilds.includes(key))
+  return isEditAllowed;
+};
+module.exports = {
+  validateSignUpData,
+  validateEditProfileData,
+};
