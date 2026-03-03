ea65349b5c5334cf4232f180d06e7c5b5fad25da
charansaigompa <gompacharansai10@gmail.com>
2026-03-03 22:30:02 +0530

    added userRoute api

--- src/app.js
+++ src/app.js
@@ -10,13 +10,14 @@ const{userAuth}=require("./middleware/auth");
 const authRouter=require("./routes/auth");
 const profileRouter=require("./routes/profile")
 const requestRouter=require("./routes/request")
+const userRouter=require("./routes/user")
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/",authRouter)
 app.use("/",profileRouter)
 app.use("/",requestRouter)
-
+app.use("/",userRouter)
 
 
 connectDB()
